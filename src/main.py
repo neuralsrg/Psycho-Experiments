a08099ba5c17501cb394d57a834de81ed9ef4f76
neuralsrg <jsetup@hotmail.com>
2023-02-17 16:02:11 +0300

    Stable

---
+++ src/main.py
@@ -0,0 +1,137 @@
+import sys
+import os
+import re
+import time
+import cv2
+import screeninfo
+from playsound import playsound
+from PyQt5 import QtWidgets, QtGui
+import windows
+
+class AppMainWindow(QtWidgets.QMainWindow, windows.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.update_user_info()
+
+        self.fullscreen_window_name = "projector" # name for cv2 window
+        self.cv2_building_delay = 0.025 # delay between building a window and first image displaying
+                                        # if it is too small, first image will not be displayed (black screen bug, not enough time to load something)
+                                        # if it is too large, it will become very noticeable (black screen before first image)
+
+        # set instructions picture (top widget on the main window)
+        scene = QtWidgets.QGraphicsScene()
+        pixmap = QtGui.QPixmap("./images/instructions.jpg")
+        item = QtWidgets.QGraphicsPixmapItem(pixmap)
+        scene.addItem(item)
+        self.instructions_graphics_viewer.setScene(scene)
+
+        # left and right buttons callbacks
+        self.button_1.clicked.connect(self.start_experiment)
+        self.button_2.clicked.connect(self.test)
+
+    def update_user_info(self):
+        '''
+        Reads user inputs (line edits) and update dictionary
+        '''
+        self.user_info = {
+            "username": self.text_input_1.text().strip().replace(' ', '_'),
+            "parameter": self.text_input_2.text().strip().replace(' ', '_'),
+            "another_parameter": self.text_input_3.text().strip().replace(' ', '_')
+        }
+
+    def user_info_is_correct(self):
+        '''
+        Check if all the inputs (line edits) were filled in with correct strings
+        '''
+        if not all(self.user_info.values()):
+            error = windows.Message("ERROR!", "Passed data shouldn't include any empty fields!")
+            error.show()
+            return False
+        elif not all([re.match(r'^[A-Za-z0-9_]+$', s.strip().replace(' ', '_')) for s in self.user_info.values()]):
+            error = windows.Message("ERROR!", "Passed fields can include nothing but letters, numbers, spaces and the '_' symbol!")
+            error.show()
+            return False
+        else:
+            return True
+
+    def build_cv2_fullscreen(self):
+        '''
+        Build screen before experiment session (loop)
+        '''
+        SCREEN_ID = 0
+        screen = screeninfo.get_monitors()[SCREEN_ID]
+        width, height = screen.width, screen.height
+        cv2.namedWindow(self.fullscreen_window_name, cv2.WND_PROP_FULLSCREEN)
+        cv2.moveWindow(self.fullscreen_window_name, screen.x - 1, screen.y - 1)
+        cv2.setWindowProperty(self.fullscreen_window_name, cv2.WND_PROP_FULLSCREEN,
+                              cv2.WINDOW_FULLSCREEN)
+        time.sleep(self.cv2_building_delay) # check init for more info
+
+    def img_audio_pair(self, img_filename: str, audio_filename: str, delay: float):
+        '''
+        Performs one stimulus (a pair of image and audio), then waits for <delay> seconds
+
+        img_filename: image path
+        audio_filename: audio path
+        delay: delay after stimulus in seconds
+        '''
+        cv2img = cv2.imread(img_filename, cv2.IMREAD_COLOR)
+        cv2.imshow(self.fullscreen_window_name, cv2img)
+        cv2.waitKey(1) # 1ms delay => sleep should be a bit less => delay = delay - 1ms
+        playsound(audio_filename)
+        time.sleep(delay)
+
+    def test(self):
+        '''
+        This function is called whenever 'Test' button is pressed.
+        Shows one test image and plays one test audio with zero delay after stimulus.
+        '''
+        self.hide()
+        self.build_cv2_fullscreen()
+        self.img_audio_pair(
+            os.path.join(".", "images", "test1.bmp"),
+            os.path.join(".", "audios", "test1.wav"),
+            0
+        )
+        cv2.destroyAllWindows()
+        self.show()
+
+    def start_experiment(self):
+        '''
+        This function is called whenever 'Start' button is pressed.
+        Builds fullscreen, then shows images and plays audios in a loop with some delays after each iteration.
+        '''
+        # user's inputs dictionary stuff
+        self.update_user_info()
+        if not self.user_info_is_correct():
+            return
+        else:
+            self.hide() # hide main application window
+
+        #self.hide() # hide main application window
+
+        # This data will be read from some files. Feel free to change it.
+        image_paths = ["./images/test1.bmp", "./images/test2.jpeg", "./images/test3.png"]
+        audio_paths = ["./audios/test1.wav", "./audios/test2.mp3", "./audios/test3.ogg"]
+        delays = [1, 1, 1]
+        delays = [(elem - 0.001) for elem in delays] # we'll use 1 ms delay from cv2.waitKey(1)
+
+        self.build_cv2_fullscreen()
+        for img_filename, audio_filename, delay in zip(image_paths, audio_paths, delays): # main experiment loop
+            self.img_audio_pair(img_filename, audio_filename, delay)
+
+        cv2.destroyAllWindows()
+        msg = windows.Message("SUCCESS!", f"Thank you for participating, {self.user_info['username']}!")
+        msg.show()
+        self.show()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = AppMainWindow()
+    window.show()
+    app.exec_()
+
+#if not imported
+if __name__ == '__main__':
+    main()
---
+++ src/windows.py
@@ -0,0 +1,91 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(750, 400)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.baseLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.baseLayout.setObjectName("baseLayout")
+
+        self.instructions_graphics_viewer = QtWidgets.QGraphicsView(self.centralwidget)
+        self.instructions_graphics_viewer.setObjectName("instructions_graphics_viewer")
+        self.baseLayout.addWidget(self.instructions_graphics_viewer)
+
+        self.inputsLayout = QtWidgets.QHBoxLayout()
+        self.inputsLayout.setObjectName("inputsLayout")
+
+        self.inputLayout_1 = QtWidgets.QVBoxLayout()
+        self.inputLayout_1.setObjectName("inputLayout_1")
+        self.input_label_1 = QtWidgets.QLabel(self.centralwidget)
+        self.input_label_1.setObjectName("input_label_1")
+        self.inputLayout_1.addWidget(self.input_label_1)
+        self.text_input_1 = QtWidgets.QLineEdit(self.centralwidget)
+        self.text_input_1.setObjectName("text_input_1")
+        self.inputLayout_1.addWidget(self.text_input_1)
+        self.inputsLayout.addLayout(self.inputLayout_1)
+
+        self.inputLayout_2 = QtWidgets.QVBoxLayout()
+        self.inputLayout_2.setObjectName("inputLayout_2")
+        self.input_label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.input_label_2.setObjectName("input_label_2")
+        self.inputLayout_2.addWidget(self.input_label_2)
+        self.text_input_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.text_input_2.setObjectName("text_input_2")
+        self.inputLayout_2.addWidget(self.text_input_2)
+        self.inputsLayout.addLayout(self.inputLayout_2)
+
+        self.inputLayout_3 = QtWidgets.QVBoxLayout()
+        self.inputLayout_3.setObjectName("inputLayout_3")
+        self.input_label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.input_label_3.setObjectName("input_label_3")
+        self.inputLayout_3.addWidget(self.input_label_3)
+        self.text_input_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.text_input_3.setObjectName("text_input_3")
+        self.inputLayout_3.addWidget(self.text_input_3)
+        self.inputsLayout.addLayout(self.inputLayout_3)
+        self.baseLayout.addLayout(self.inputsLayout)
+
+        self.buttonsLayout = QtWidgets.QHBoxLayout()
+        self.buttonsLayout.setObjectName("buttonsLayout")
+        self.button_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_1.setObjectName("button_1")
+        self.buttonsLayout.addWidget(self.button_1)
+        self.button_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.button_2.setObjectName("button_2")
+        self.buttonsLayout.addWidget(self.button_2)
+        self.baseLayout.addLayout(self.buttonsLayout)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 634, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "PsychoApp"))
+        self.input_label_1.setText(_translate("MainWindow", "Username"))
+        self.input_label_2.setText(_translate("MainWindow", "Parameter"))
+        self.input_label_3.setText(_translate("MainWindow", "Another parameter"))
+        self.button_1.setText(_translate("MainWindow", "Start"))
+        self.button_2.setText(_translate("MainWindow", "Test"))
+
+class Message:
+    def __init__(self, msg1="ERROR!", msg2="ERROR!"):
+        self.msg = QtWidgets.QMessageBox()
+        self.msg.setIcon(QtWidgets.QMessageBox.Critical)
+        self.msg.setWindowTitle(msg1)
+        self.msg.setText(msg2)
+    
+    def show(self):
+        self.msg.exec_()
